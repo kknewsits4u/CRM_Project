834abd246fb3a2c822bddc0ee5d3cccb110ab197
Krishna Kant Sharma <kknewsits4u@gmail.com>
2025-04-26 18:58:27 +0530

    logib page code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom';  
 import LoginPage from './Pages/LoginPage';
+import SignUp from './Pages/SignUp';
 
 
 function App() {
   return (
-    <Routes>
+     <Routes>
       <Route path="/" element={<LoginPage />} />
     </Routes>
   );
